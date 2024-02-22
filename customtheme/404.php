ab92da462ad1f62f2ec37dd9616e1a278c5503e6
henristy <henristy2010@gmail.com>
2024-02-22 17:01:33 +0100

    cc

--- customtheme/404.php
+++ customtheme/404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<main class='404 d-flex justify-content-center align-items-center'>
+<main class='404 d-flex justify-content-center align-items-center h-100'>
     <h1 class='text-center'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-primary" href="#home">Go back home</button></h1>
 </main>
 
