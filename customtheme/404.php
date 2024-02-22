7afdb90d49b0db91fc6ae8f68b4a3f0e9a49819d
henristy <henristy2010@gmail.com>
2024-02-22 17:21:20 +0100

    dhsh

--- customtheme/404.php
+++ customtheme/404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <main class=''>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img" class='w-100 h-100'>
-    <h1 class='position-absolute z-3 text-center top-50'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-primary" href="#home">Go back home</button></h1>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img" class='w-100 h-50'>
+    <h1 class='position-absolute z-3 text-center start-50 top-50'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-primary" href="#home">Go back home</button></h1>
 </main>
 
 
