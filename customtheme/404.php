b613b96bbb6413d90c4f2e910c87831b4938c5fd
henristy <henristy2010@gmail.com>
2024-02-22 16:46:40 +0100

    img fix

--- customtheme/404.php
+++ customtheme/404.php
@@ -3,7 +3,8 @@
 <main>
     <div class="container mt-5">
         <div>
-            <img src="assets/img/404.jpg" class="w-100 mb-3">
+            <!-- <img src="/assets/img/404.jpg" class="w-100 mb-3"> -->
+            <img class="w-100 mb-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 image">
         </div>
         <h2 class='text-center'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-link" href="#home">Go back home</button></h2>
     </div>
