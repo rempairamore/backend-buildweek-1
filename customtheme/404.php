040e888f7a9a2d80320cc94a40a7eb342ac4d826
henristy <henristy2010@gmail.com>
2024-02-22 17:26:18 +0100

    diskco

--- customtheme/404.php
+++ customtheme/404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <main class=''>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img" class='w-100' height="900px">
-    <h1 class='position-absolute z-3 text-white bottom-100'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-light" href="#home">Go back home</button></h1>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img" class='w-100' height="1000px">
+    <h1 class='position-absolute z-3 text-white bottom-75'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-light" href="#home">Go back home</button></h1>
 </main>
 
 
