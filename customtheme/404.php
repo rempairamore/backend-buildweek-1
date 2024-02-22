d020743a4b2cf7a68fd563076a7be61a4d0d956e
henristy <henristy2010@gmail.com>
2024-02-22 16:27:38 +0100

    preview 404 page

---
+++ customtheme/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main>
+    <div class="container mt-5">
+        <div>
+            <img src="assets/img/404.jpg" class="w-100 mb-3">
+        </div>
+        <h2 class='text-center'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-link" href="#home">Go back home</button></h2>
+    </div>
+</main>
+
+
+
+
+
+<?php get_footer(); ?>
