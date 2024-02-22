52f5506b111a55e1144a93749b71c7ddcf690d5a
henristy <henristy2010@gmail.com>
2024-02-22 17:38:10 +0100

    finellaa

--- customtheme/404.php
+++ customtheme/404.php
@@ -3,11 +3,10 @@
 <main class=''>
     <div class="card text-bg-dark">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img" class='w-100'
-            height="1000px">
-        <div class="card-img-overlay d-flex justify-content-center align-items-center">
+            height="1080px">
+        <div class="card-img-overlay d-flex justify-content-center align-items-end">
             <h1 class='card-title text-info'>Sorry but we couldn't find the page you were looking for!
                 <button class="btn btn-outline-light" href="#home">Go back home</button></h1>
-
         </div>
     </div>
 
