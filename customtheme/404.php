8fc332130f6b4cde2e0b2e3ab9f9f66dd4b16707
henristy <henristy2010@gmail.com>
2024-02-22 17:16:02 +0100

    ddh

--- customtheme/404.php
+++ customtheme/404.php
@@ -2,7 +2,7 @@
 
 <main class=''>
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img" class='w-100'>
-    <h1 class='position-fixed top-0 bottom-0 start-0 end-0'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-primary" href="#home">Go back home</button></h1>
+    <h1 class='position-fixed top-50'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-primary" href="#home">Go back home</button></h1>
 </main>
 
 
