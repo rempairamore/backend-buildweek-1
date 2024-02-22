ecb10d86d91afa56a2570f9672772ceec8bcbe3b
henristy <henristy2010@gmail.com>
2024-02-22 16:59:55 +0100

    size up img

--- customtheme/404.php
+++ customtheme/404.php
@@ -1,13 +1,7 @@
 <?php get_header(); ?>
 
-<main>
-    <div class="container mt-5">
-        <div>
-            <!-- <img src="/assets/img/404.jpg" class="w-100 mb-3"> -->
-            <img class="w-100 mb-3" src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 image">
-        </div>
-        <h2 class='text-center'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-link" href="#home">Go back home</button></h2>
-    </div>
+<main class='404 d-flex justify-content-center align-items-center'>
+    <h1 class='text-center'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-primary" href="#home">Go back home</button></h1>
 </main>
 
 
