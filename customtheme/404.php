7a72409f43160de5b59314b4ffd1212d9d8bca28
henristy <henristy2010@gmail.com>
2024-02-22 17:40:47 +0100

    cxcxcxc

--- customtheme/404.php
+++ customtheme/404.php
@@ -5,7 +5,7 @@
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img" class='w-100'
             height="1020px">
         <div class="card-img-overlay d-flex justify-content-center align-items-start pt-5">
-            <h1 class='card-title text-info'>Sorry but we couldn't find the page you were looking for!
+            <h1 class='card-title'>Sorry but we couldn't find the page you were looking for!
                 <button class="btn btn-outline-light" href="#home">Go back home</button></h1>
         </div>
     </div>
