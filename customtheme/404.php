0d3fba8b8a75a66b371e7c6ed630fba2a98ce1b0
henristy <henristy2010@gmail.com>
2024-02-22 17:04:20 +0100

    xc

--- customtheme/404.php
+++ customtheme/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main class='404 d-flex justify-content-center align-items-center h-100'>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/404.jpg" alt="404 img">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/404.jpg" alt="404 img">
     <h1 class='text-center'>Sorry but we couldn't find the page you were looking for! <button class="btn btn-outline-primary" href="#home">Go back home</button></h1>
 </main>
 
