849259066e58040cf14edd92f04e502f6a14d41f
Fraokk <occhinerifrancesco@gmail.com>
2024-02-23 14:36:16 +0100

    post thumbnail

--- customtheme/single.php
+++ customtheme/single.php
@@ -6,17 +6,29 @@ if (have_posts()) {
         the_post();
         ?>
         <div style="position: relative;">
-        <img class="imgArticle" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="...">
+        <?php if (has_post_thumbnail()) { ?>
+            <img class="imgArticle" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="...">
+        <?php } ?>
         <div>
         <h1 class="articleTitle fw-bold text-light"><?php the_title(); ?></h1>
             </div>
         </div>
         <div class="article-content">
-            <?php the_content(); ?>
+            <?php 
+            
+            if (has_post_thumbnail()) {
+                $content = apply_filters( 'the_content', get_the_content() );
+                $content = preg_replace( '/<img[^>]+>/', '', $content, 1 );
+                echo $content;
+            } else {
+                the_content();
+            }
+            ?>
         </div>
         <?php
     }
 }
 ?>
+
 
 <?php get_footer(); ?>
