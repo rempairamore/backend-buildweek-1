cc0555b11093ba0e60dda95c721bdfb8defdaa80
AleDC91 <ale91.dallas@gmail.com>
2024-02-23 10:09:44 +0100

    content

--- customtheme/page.php
+++ customtheme/page.php
@@ -1,1 +1,18 @@
-culo
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php
+        // Loop principale di WordPress
+        while (have_posts()) :
+            the_post();
+
+            // Contenuto della pagina
+            the_content();
+
+        endwhile; // Fine del loop principale
+        ?>
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
