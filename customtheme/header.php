fd63611a5f8f8c4f1baa0c61670e26d8b1ce589f
AleDC91 <ale91.dallas@gmail.com>
2024-02-23 10:52:52 +0100

    primary-menu

--- customtheme/header.php
+++ customtheme/header.php
@@ -21,7 +21,7 @@
                         <?php 
           
                        wp_nav_menu(array(
-                           'theme_location' => 'primary-menu',
+                           'theme_location' => 'primary',
                            'container' => false,
                            'menu_class' => '',
                            'fallback_cb' => '__return_false',
