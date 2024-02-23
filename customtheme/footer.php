0934dae00e50850c13af21921d4764ac66bc5a8d
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-23 11:43:53 +0100

    footer_patch

--- customtheme/footer.php
+++ customtheme/footer.php
@@ -1,4 +1,26 @@
-    pippo
+    <footer>
+        <div class=d-flex>
+            <div>
+                <ul>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>2</li>
+                    <li>2</li>
+                    <li>2</li>
+                    <li>2</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            &copy
+        </div>
+    </footer>
 <?php wp_footer(); ?>
 </body>
 
