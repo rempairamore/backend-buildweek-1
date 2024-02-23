25b88815ab691a19c3125279b6961f51d3bb59cf
Fraokk <occhinerifrancesco@gmail.com>
2024-02-23 12:34:48 +0100

    single article

---
+++ customtheme/single.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<?php
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        ?>
+        <div style="position: relative;">
+        <img class="imgArticle" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="...">
+        <div>
+        <h1 class="articleTitle fw-bold"><?php the_title(); ?></h1>
+            </div>
+        </div>
+        <div class="article-content">
+            <?php the_content(); ?>
+        </div>
+        <?php
+    }
+}
+?>
+
+<?php get_footer(); ?>
