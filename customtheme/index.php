cd5b6573b944cc5500716e6486e57f7d86b79a47
AleDC91 <ale91.dallas@gmail.com>
2024-02-23 15:29:41 +0100

    sdfghj

--- customtheme/index.php
+++ customtheme/index.php
@@ -40,7 +40,7 @@
 
 
     <div class="griglia">
-        <section class="articoli d-flex container row justify-content-evenly">
+        <section class="articoli d-flex row justify-content-evenly">
 
             <?php
             $args = array(
@@ -54,20 +54,23 @@
                 $count = 0;
                 while ($custom_query->have_posts()) {
                     $count++;
-                    $custom_query->the_post();?>
-                    <article class="single-article-home col-12 col-lg-6 col-xxl-4 p-3 <?php ($count == 3) ? print("d-lg-none d-xxl-block") : ""?>">
-                    <div class="article-thumbnail-home">
-                        <?php
-                        the_post_thumbnail('custom-size-thumbnail');
-                        ?>
-                    </div>
-                    <div class="p-2 article-info-home">
-                        <h5 class="article-title-home"><?php the_title(); ?></h5>
-                        <p class="article-author-home"><?php the_author(); ?></p>
-                       <span class="ms-2"><a class="article-link-home" href="<?php the_permalink(); ?>"> ...leggi</a></span> 
-                    </div>
-                </article>
-              <?php   }
+                    $custom_query->the_post(); ?>
+
+                    <article class="single-article-home d-flex flex-column align-items-center align-items-lg-center col-12 col-lg-6 col-xxl-4 p-3 <?php ($count == 3) ? print("d-lg-none d-xxl-block") : "" ?>">
+                        <div class="article-thumbnail-home">
+                            <?php
+                            the_post_thumbnail('custom-size-thumbnail');
+                            ?>
+                        </div>
+                        <div class="p-2 article-info-home">
+                            <h5 class="article-title-home"><?php the_title(); ?></h5>
+                            <p class="article-author-home"><?php the_author(); ?></p>
+                            <span class="ms-2"><a class="article-link-home" href="<?php the_permalink(); ?>"> ...leggi</a></span>
+                        </div>
+                    </article>
+
+
+            <?php   }
             }
             ?>
 
