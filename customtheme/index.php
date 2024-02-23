b103130a9b50b445580a8e013597aadae52a9785
AleDC91 <ale91.dallas@gmail.com>
2024-02-23 15:01:17 +0100

    dtyjdghj

--- customtheme/index.php
+++ customtheme/index.php
@@ -44,7 +44,7 @@
 
             <?php
             $args = array(
-                'posts_per_page' => 2, // Retrieve only 3 posts
+                'posts_per_page' => 3, // Retrieve only 3 posts
                 'order'          => 'DESC', // Order posts in descending order (latest posts first)
             );
 
@@ -55,15 +55,16 @@
                 while ($custom_query->have_posts()) {
                     $count++;
                     $custom_query->the_post();?>
-                    <article class="single-article-home d-flex col-12 col-lg-6 col-xxl-4 p-3 <?php ($count == 2) ? print("d-lg-none d-xxl-block") : ""?>">
+                    <article class="single-article-home d-flex col-12 col-lg-6 col-xxl-4 p-3 <?php ($count == 3) ? print("d-lg-none d-xxl-block") : ""?>">
                     <div class="article-thumbnail-home">
                         <?php
                         the_post_thumbnail('custom-size-thumbnail');
                         ?>
                     </div>
-                    <div class="p-2">
-                        <h5><?php the_title(); ?></h5>
-                        <p><?php the_excerpt(); ?></p>
+                    <div class="p-2 article-info-home">
+                        <h5 class="article-title-home"><?php the_title(); ?></h5>
+                        <p class="article-excerpt-home"><?php the_excerpt(); ?></p>
+                        <a class="article-link-home" href="<?php the_permalink(); ?>"> ...visualizza</a>
                     </div>
                 </article>
               <?php   }
@@ -71,17 +72,7 @@
             ?>
 
 
- 
 
-            <article class="single-article-home d-flex col-12 col-lg-4">
-                <div class="article-thumbnail-home">
-
-                </div>
-                <div>
-                    <h5>titolo articolo</h5>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni dolore facere qui, optio ducimus fugiat similique quia iure nesciunt veritatis dolorum eligendi reprehenderit incidunt labore earum asperiores nemo eveniet ea.</p>
-                </div>
-            </article>
         </section>
         <aside class="side text-center">
             <?php
