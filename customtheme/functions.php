a636345f232770a1f78a24c461bcf3a606168163
AleDC91 <ale91.dallas@gmail.com>
2024-02-23 14:14:09 +0100

    diothumbnail

--- customtheme/functions.php
+++ customtheme/functions.php
@@ -23,17 +23,15 @@ add_action('wp_enqueue_scripts', 'load_bootstrap_scripts');
 
 // ADD FEATURES
 
-function custom_theme_setup() {
-    // Add theme support for post thumbnails
+function ale_theme_support()
+{
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
     add_theme_support('post-thumbnails');
 }
 
-add_action('after_setup_theme', 'custom_theme_setup');
-function gruppo4_theme_support(){
-    add_theme_support('title-tag');
-}
+add_action('after_setup_theme', 'ale_theme_support');
 
-add_action('after_setup_theme', 'gruppo4_theme_support');
 
 //end add features
 
--- customtheme/single.php
+++ customtheme/single.php
@@ -1,1 +1,5 @@
 <?php the_title() ?>
+
+<div>
+
+</div<?php the_post_thumbnail() ?>
