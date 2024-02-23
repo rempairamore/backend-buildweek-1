4671023ae20d63b476a5244c144be93946244b78
henristy <henristy2010@gmail.com>
2024-02-23 12:51:05 +0100

    ytr

--- customtheme/page.php
+++ customtheme/page.php
@@ -11,37 +11,43 @@ get_header();
 ?>
 
 <div class="container mt-5 pt-5">
+    <header class="entry-header position-relative">
+        <?php if (has_post_thumbnail()): ?>
+            <div class="featured-image position-relative">
+                <?php the_post_thumbnail('full'); ?>
+                <div class="overlay d-flex justify-content-center align-items-center">
+                    <?php the_title('<h1 class="entry-title text-center">', '</h1>'); ?>
+                </div>
+            </div>
+        <?php else: ?>
+            <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+        <?php endif; ?>
+    </header><!-- .entry-header -->
+
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-7">
             <!-- Main Content Area -->
             <main id="main" class="site-main">
                 <?php
                 // Start the loop.
-                while (have_posts()) :
+                while (have_posts()):
                     the_post();
                     ?>
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <header class="entry-header">
-                            <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-                            <?php if (has_post_thumbnail()) : ?>
-                                <div class="featured-image">
-                                    <?php the_post_thumbnail('full'); ?>
-                                </div>
-                            <?php endif; ?>
-                        </header><!-- .entry-header -->
+
 
                         <div class="entry-content">
                             <?php the_content(); ?>
                         </div><!-- .entry-content -->
                     </article><!-- #post-<?php the_ID(); ?> -->
 
-                <?php
-                // End of the loop.
+                    <?php
+                    // End of the loop.
                 endwhile;
                 ?>
             </main><!-- #main -->
         </div>
-        <div class="col-md-3">
+        <div class="col-md-5">
             <!-- Right Sidebar -->
             <div id="sidebar">
                 <div id="sidebar-widget">
