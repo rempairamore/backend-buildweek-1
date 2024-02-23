4fe26d16a73c51885d4c2e542ea325467a68909b
AleDC91 <ale91.dallas@gmail.com>
2024-02-23 17:42:11 +0100

    mhdgnd

--- customtheme/index.php
+++ customtheme/index.php
@@ -2,22 +2,34 @@
 
 <main>
    
-   
+<?php
+// Recupera l'ID della pagina che contiene le immagini del carousel
+$carousel_page_id = 129; // Sostituisci 123 con l'ID effettivo della pagina
+
+// Recupera gli ID delle immagini caricate come allegati della pagina
+$image1_id = get_post_meta($carousel_page_id, 'image1_id', true);
+$image2_id = get_post_meta($carousel_page_id, 'image2_id', true);
+$image3_id = get_post_meta($carousel_page_id, 'image3_id', true);
+
+// Ottieni le URL delle immagini utilizzando gli ID degli allegati
+$image1_url = wp_get_attachment_image_src($image1_id, 'full')[0];
+$image2_url = wp_get_attachment_image_src($image2_id, 'full')[0];
+$image3_url = wp_get_attachment_image_src($image3_id, 'full')[0];
+?>
     <div class="container-fluid hero">
         <div id="carouselHome" class="carousel slide">
 
-
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="<?php echo esc_url($image1_url); ?>" class="d-block w-100" alt="Immagine 1">
-                </div>
-                <div class="carousel-item">
-                    <img src="<?php echo esc_url($image2_url); ?>" class="d-block w-100" alt="Immagine 2">
-                </div>
-                <div class="carousel-item">
-                    <img src="<?php echo esc_url($image3_url); ?>" class="d-block w-100" alt="Immagine 3">
-                </div>
-            </div>
+        <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="<?php echo esc_url($image1_url); ?>" class="d-block w-100" alt="Immagine 1">
+    </div>
+    <div class="carousel-item">
+        <img src="<?php echo esc_url($image2_url); ?>" class="d-block w-100" alt="Immagine 2">
+    </div>
+    <div class="carousel-item">
+        <img src="<?php echo esc_url($image3_url); ?>" class="d-block w-100" alt="Immagine 3">
+    </div>
+</div>
 
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
