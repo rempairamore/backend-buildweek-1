ce793c0f3bcd957b9c2e29ab67734b93326e6dba
Fraokk <occhinerifrancesco@gmail.com>
2024-02-23 17:34:57 +0100

    sidebar fix

--- customtheme/single.php
+++ customtheme/single.php
@@ -4,34 +4,36 @@
 if (have_posts()) {
     while (have_posts()) {
         the_post();
-        ?>
+?>
         <div style="position: relative;">
-        <?php if (has_post_thumbnail()) { ?>
-            <img class="imgArticle" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="...">
-        <?php } ?>
-        <div>
-        <h1 class="articleTitle fw-bold text-light"><?php the_title(); ?></h1>
+            <?php if (has_post_thumbnail()) { ?>
+                <img class="imgArticle" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="...">
+            <?php } ?>
+            <div>
+                <h1 class="articleTitle fw-bold text-light"><?php the_title(); ?></h1>
             </div>
         </div>
-        <div class="article-content">
-            <?php 
-            
-            if (has_post_thumbnail()) {
-                $content = apply_filters('the_content', get_the_content());
-                $content = preg_replace('/<img[^>]+>/', '', $content, 1);
-                echo '<div class="row"> 
-                        <div class="col-8">' . $content . '</div>
-                        <div class="col-4">' . get_sidebar() . '</div>
-                      </div>';
-            } else {
-                the_content();
-            }
-            ?>
+        <div class="row">
+            <div class="col-8">
+                <div class="article-content">
+                    <?php
+                    if (has_post_thumbnail()) {
+                        $content = apply_filters('the_content', get_the_content());
+                        $content = preg_replace('/<img[^>]+>/', '', $content, 1);
+                        echo $content;
+                    } else {
+                        the_content();
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="col-4">
+                <?php get_sidebar(); ?>
+            </div>
         </div>
-        <?php
+<?php
     }
 }
 ?>
-
 
 <?php get_footer(); ?>
