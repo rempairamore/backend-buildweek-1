d85f057e0f16324ceb0646e3321cd3d8d56b6aa8
henristy <henristy2010@gmail.com>
2024-02-23 11:20:40 +0100

    ddi

--- customtheme/page.php
+++ customtheme/page.php
@@ -34,6 +34,7 @@ get_header();
                 // Start the loop.
                 while (have_posts()) :
                     the_post();
+                    the_content();
                     // Include the page content template.
                     get_template_part('template-parts/content', 'page');
                 // End of the loop.
