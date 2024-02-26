4159b9d36bf31f11fb0b84d19f47200a35993c49
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 14:29:15 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -124,36 +124,6 @@
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Wintry Mountain Landscape"
                         />
-                    </div>
-
-                    <div class="col-lg-4 mb-4 mb-lg-0">
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Mountains in the Clouds"
-                        />
-
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Boat on Calm Water"
-                        />
-                    </div>
-
-                    <div class="col-lg-4 mb-4 mb-lg-0">
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Waves at Sea"
-                        />
-
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Yosemite National Park"
-                        />
-                    </div>
-                    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
                         <img
                         src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
                         class="w-100 shadow-1-strong rounded mb-4"
@@ -179,6 +149,17 @@
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Boat on Calm Water"
                         />
+                        <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                        class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Mountains in the Clouds"
+                        />
+
+                        <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Boat on Calm Water"
+                        />
                     </div>
 
                     <div class="col-lg-4 mb-4 mb-lg-0">
@@ -193,7 +174,19 @@
                         class="w-100 shadow-1-strong rounded mb-4"
                         alt="Yosemite National Park"
                         />
+                        <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                        class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Waves at Sea"
+                        />
+
+                        <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                        class="w-100 shadow-1-strong rounded mb-4"
+                        alt="Yosemite National Park"
+                        />
                     </div>
+
 
                 </div>
             </div>
