cdbfc7e6c62f196637fafa3ff36bffce5f82a717
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 23:11:45 +0100

    article pacth

--- customtheme/single.php
+++ customtheme/single.php
@@ -9,14 +9,13 @@ if (have_posts()) {
             <?php if (has_post_thumbnail()) { ?>
                 <img class="imgArticle" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="...">
             <?php } ?>
-         
-            <div class="titoloArticolo position-absolute bottom-0 start-0">
+            <div class="titoloArticolo text-center position-absolute bottom-0 start-0 w-100">
                 <h1><?php the_title(); ?></h1>
             </div>
         </div>
 
         <div class="container">
-            <div class="row mt-5 p-5">
+            <div class="row p-5">
                 <div class="col-sm-8">
                     <div class="article-content">
                         <?php
