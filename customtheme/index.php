2be408bd6d818c27c30f2011b234dc8a917ebd4f
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 14:46:17 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -98,19 +98,8 @@
         </aside>
         <section class="gallery text-center">
             galleryddd
-            <!-- <div class="row">
-                <div class="col">
-                    <img src="https://www.my-personaltrainer.it/2023/11/24/gatto-europeo_900x760.jpeg" alt="" >
-                </div>
-                <div class="col">
-                    <img src="https://www.my-personaltrainer.it/2023/11/24/gatto-europeo_900x760.jpeg" alt="" >
-                </div>
-                <div class="col">
-                    <img src="https://www.my-personaltrainer.it/2023/11/24/gatto-europeo_900x760.jpeg" alt="" >
-                </div>
-            </div> -->
             <div class="container">
-                <div class="row">
+                <!-- <div class="row">
 
                     <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
                         <img
@@ -187,6 +176,72 @@
                         />
                     </div>
 
+
+                </div> -->
+                    <div class="movie-gallery mx-md-5 mb-5 mt-4">
+                    <h5 class="text-light mt-2 mb-2">Trending Now</h5>
+                        <div id="trending-now" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item">
+                                    <div class="movie-row">
+                                        <div class="row g-1">
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media0.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media1.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media2.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media3.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media4.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media5.jpg">
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item active">
+                                    <div class="movie-row">
+                                        <div class="row g-1">
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media0.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media1.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media2.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media3.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media4.jpg">
+                                            </div>
+                                            <div class="col-md-2">
+                                                <img class="img-fluid movie-cover" src="./assets/media/media5.jpg">
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#trending-now" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#trending-now" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
 
                 </div>
             </div>
