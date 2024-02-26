ccde46361af78f04c9cbee3ff7ad2b7580332c8c
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 19:02:27 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -105,16 +105,13 @@
                     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="10000">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="d-block object-fit-contain" alt="...">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                                <div style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp'); ">
-                                <!-- <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block object-fit-contain" alt="..."> -->
-                                </div>
-                                
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block object-fit-contain" alt="...">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block w-100" alt="...">
                             </div>
                         </div>
                         
