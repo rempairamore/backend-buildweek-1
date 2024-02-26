edf51382950dad0cdc2833fc9c5acf21eabab962
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 17:05:16 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -100,7 +100,7 @@
             galleryddd
             <div class="d-block d-sm-none">
                 small
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-bs-interval="10000">
                             <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="d-block w-100" alt="...">
@@ -112,7 +112,7 @@
                             <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block w-100" alt="...">
                         </div>
                     </div>
-
+                    
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
