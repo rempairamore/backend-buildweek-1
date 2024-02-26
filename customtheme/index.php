db61acf68dc3f543cc3250bde03e9b755868ba74
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-27 00:59:44 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -86,7 +86,6 @@
                                         ...leggi</a></span>
                             </div>
                         </div>
-                       
                     </article>
 
 
