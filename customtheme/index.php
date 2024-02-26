4327a6cfbb4515ea3f591892272fda2df367fdf4
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 18:35:02 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -96,21 +96,22 @@
             ?>
 
         </aside>
+
         <section class="gallery text-center">
             galleryddd
             <div class="d-lg-none d-xl-block">
                 small
                 <div class="mx-auto">
                     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
+                        <div class="carousel-inner text-center">
                             <div class="carousel-item active" data-bs-interval="10000">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="d-block object-fit-contain" alt="...">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block object-fit-contain" alt="...">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block object-fit-contain" alt="...">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="d-block w-100" alt="...">
                             </div>
                         </div>
                         
@@ -208,6 +209,7 @@
 
                 </div>
             </div>
+            <!-- --modale--- -->
         </section>
 
     </div>
