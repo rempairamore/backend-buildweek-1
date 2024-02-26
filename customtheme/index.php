bcee7b6c2f97c6c0edc7e2454ad8b4422efb6d7f
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 14:58:22 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -178,70 +178,70 @@
 
 
                 </div> -->
-                    <div class="movie-gallery mx-md-5 mb-5 mt-4">
-                    <h5 class="text-light mt-2 mb-2">Trending Now</h5>
-                        <div id="trending-now" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item">
-                                    <div class="movie-row">
-                                        <div class="row g-1">
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
-                                            </div>
-
+                   
+                    
+                    <div id="trending-now" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item">
+                                <div class="movie-row">
+                                    <div class="row g-1">
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="carousel-item active">
-                                    <div class="movie-row">
-                                        <div class="row g-1">
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="./assets/media/media0.jpg">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="./assets/media/media1.jpg">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="./assets/media/media2.jpg">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="./assets/media/media3.jpg">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="./assets/media/media4.jpg">
-                                            </div>
-                                            <div class="col-md-2">
-                                                <img class="img-fluid movie-cover" src="./assets/media/media5.jpg">
-                                            </div>
-
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
                                         </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#trending-now" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#trending-now" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
+                            <div class="carousel-item active">
+                                <div class="movie-row">
+                                    <div class="row g-1">
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="./assets/media/media0.jpg">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="./assets/media/media1.jpg">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="./assets/media/media2.jpg">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="./assets/media/media3.jpg">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="./assets/media/media4.jpg">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <img class="img-fluid movie-cover" src="./assets/media/media5.jpg">
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#trending-now" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#trending-now" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
 
                 </div>
             </div>
