f5741351a083a1df19a7b880146ba7196423e782
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-26 13:56:58 +0100

    gallery patch

--- customtheme/index.php
+++ customtheme/index.php
@@ -109,7 +109,7 @@
                     <img src="https://www.my-personaltrainer.it/2023/11/24/gatto-europeo_900x760.jpeg" alt="" >
                 </div>
             </div> -->
-            <div class="container">
+            <div class="">
                 <div class="row">
                     <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
                         <img
@@ -139,32 +139,6 @@
                         />
                     </div>
 
-                    <div class="col-lg-4 mb-4 mb-lg-0">
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Waves at Sea"
-                        />
-
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Yosemite National Park"
-                        />
-                    </div>
-                    <div class="col-lg-4 mb-4 mb-lg-0">
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Waves at Sea"
-                        />
-
-                        <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-                        class="w-100 shadow-1-strong rounded mb-4"
-                        alt="Yosemite National Park"
-                        />
-                    </div>
                     <div class="col-lg-4 mb-4 mb-lg-0">
                         <img
                         src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
