5add9d75266608a41be65da7f3ae9e3c5228f0f3
AleDC91 <ale91.dallas@gmail.com>
2024-02-27 10:39:42 +0100

    asdf

--- customtheme/page.php
+++ customtheme/page.php
@@ -1,16 +1,46 @@
 <?php
-/**
- * The template for displaying individual pages.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-page
- *
- * @package YourTheme
- */
+$post_type = get_post_type();
 
-get_header();
+if ($post_type == 'page') {
+    get_header();
+
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
 ?>
+            <div style="position: relative;">
+                <?php if (has_post_thumbnail()) { ?>
+                    <img class="imgArticle" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="...">
+                <?php } ?>
+                <div class="titoloArticolo text-center position-absolute bottom-0 start-0 w-100">
+                    <h1><?php the_title(); ?></h1>
+                </div>
+            </div>
 
-<?php the_content(); ?>
+            <div class="container">
+                <div class="row p-5">
+                    <div class="col-sm-8">
+                        <div class="article-content">
+                            <?php
+                            if (has_post_thumbnail()) {
+                                $content = apply_filters('the_content', get_the_content());
+                                $content = preg_replace('/<img[^>]+>/', '', $content, 1);
+                                echo $content;
+                            } else {
+                                the_content();
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <?php dynamic_sidebar('right-sidebar'); ?>
+                    </div>
 
-<?php get_footer(); ?>
+                </div>
+            </div>
+    <?php
+        }
+    }
+}
+
 
