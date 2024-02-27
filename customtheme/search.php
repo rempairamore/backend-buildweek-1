60d02fdeb3a10c5ac0c369b85dd76a8afda4e060
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-27 12:06:20 +0100

    search

--- customtheme/search.php
+++ customtheme/search.php
@@ -14,7 +14,7 @@
                     <img class="imgArticle" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fviaggi.corriere.it%2Feuropa%2Fislanda%2F&psig=AOvVaw02v5NBOA1SZcxhz0FseSjk&ust=1709117229220000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIC8kvyry4QDFQAAAAAdAAAAABAE" alt="...">
              
                 <div class="titoloArticolo text-center position-absolute bottom-0 start-0 w-100">
-                    <h1><?php $wp_query['query']['s'] ?></h1>
+                    <h1><?php echo $wp_query['query']['s']; ?></h1>
                 </div>
             </div>
 
