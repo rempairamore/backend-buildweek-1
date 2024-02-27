4f2b86eb425645aa1e6be2126c8b4061f92a721c
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-27 12:28:07 +0100

    search

--- customtheme/search.php
+++ customtheme/search.php
@@ -26,6 +26,7 @@
                     ?>
                         <div class="serach-article-content">
                             <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <p><?php the_author(); ?></p>
                             <?php the_excerpt(); ?>
                         </div>
                     <?php
