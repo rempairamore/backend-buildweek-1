518d5227d4cedd355fce1407697d654a2aff159f
AleDC91 <ale91.dallas@gmail.com>
2024-02-27 09:55:18 +0100

    articolo doppio

--- customtheme/index.php
+++ customtheme/index.php
@@ -99,23 +99,6 @@ $descrizione = urldecode($wpdb->get_var($wpdb->prepare("SELECT description FROM 
                                         ...leggi</a></span>
                             </div>
 
-                    <article class="single-article-home d-flex flex-column align-items-center align-items-lg-start col-12 col-lg-6 col-xxl-4 p-3 <?php ($count == 3) ? print("d-lg-none d-xxl-block") : "" ?>">
-                        <div class="article-thumbnail-home">
-                            <?php
-                            the_post_thumbnail('custom-size-thumbnail');
-                            ?>
-                        </div>
-                        <div class="p-2 article-info-home">
-                            <h5 class="article-title-home">
-                                <?php the_title(); ?>
-                            </h5>
-                            <p class="article-author-home">
-                                <?php the_author(); ?>
-                            </p>
-                            <span class="ms-2"><a class="article-link-home" href="<?php the_permalink(); ?>">
-                                    ...leggi</a></span>
-
-                        </div>
                     </article>
 
 
