fc8557616c2e725943eab943b4185126529a012f
AleDC91 <ale91.dallas@gmail.com>
2024-02-27 15:27:41 +0100

    vsdfvsdf

--- customtheme/header.php
+++ customtheme/header.php
@@ -13,10 +13,13 @@
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
 
-                    <?php if (function_exists('the_custom_logo')) {
-                        $custom_logo_id = get_theme_mod('custom_logo');
-                        $logo = wp_get_attachment_image_src($custom_logo_id);
-                    } ?>
+                    <?php
+                    // Display the custom logo
+                    if (function_exists('the_custom_logo')) {
+                        the_custom_logo();
+                    }
+                    ?>
+
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
