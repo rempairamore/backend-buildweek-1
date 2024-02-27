cc2982aa162a54ad6230f95784f503f0935ddcf6
AleDC91 <ale91.dallas@gmail.com>
2024-02-27 15:26:19 +0100

    vsdfsfd

--- customtheme/header.php
+++ customtheme/header.php
@@ -11,30 +11,36 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">
+
+                    <?php if (function_exists('the_custom_logo')) {
+                        $custom_logo_id = get_theme_mod('custom_logo');
+                        $logo = wp_get_attachment_image_src($custom_logo_id);
+                    } ?>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0"> -->
-                        <?php 
-          
-                       wp_nav_menu(array(
-                           'theme_location' => 'primary',
-                           'container' => false,
-                           'menu_class' => '',
-                           'fallback_cb' => '__return_false',
-                           'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-lg-0 %2$s">%3$s</ul>',
-                           'depth' => 2,
-                           'walker' => new Bootstrap_5_WP_Nav_Menu_Walker()
-                       ));
-                   ?>
-                            
-                      
+                    <?php
+
+                    wp_nav_menu(array(
+                        'theme_location' => 'primary',
+                        'container' => false,
+                        'menu_class' => '',
+                        'fallback_cb' => '__return_false',
+                        'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-lg-0 %2$s">%3$s</ul>',
+                        'depth' => 2,
+                        'walker' => new Bootstrap_5_WP_Nav_Menu_Walker()
+                    ));
+                    ?>
 
 
 
-                        <!-- <li class="nav-item">
+
+
+                    <!-- <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
