49342d06a0dccc90664c73fe04cb052f612a4926
Francesco Cecca <fcecca3@gmail.com>
2024-02-27 10:35:56 +0100

    prova

--- customtheme/index.php
+++ customtheme/index.php
@@ -57,6 +57,39 @@ $descrizione = urldecode($wpdb->get_var($wpdb->prepare("SELECT description FROM 
 
     <section class="promozioni">
 
+        <div class="row">
+            <!-- Card 1 -->
+            <div class="col-sm-12 col-md-4 mb-4">
+                <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top rounded-circle mx-auto d-block mt-3" alt="..." style="width: 100px; height: 100px;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Sconti Primavera in Europa</h5>
+                    <p class="card-text">Approfitta ora degli sconti fino al 30% sui nostri tour primaverili in Europa. Offerta limitata per esplorare le capitali europee in fiore</p>
+                </div>
+                </div>
+            </div>
+            <!-- Card 2 -->
+            <div class="col-sm-12 col-md-4 mb-4">
+                <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top rounded-circle mx-auto d-block mt-3" alt="..." style="width: 100px; height: 100px;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Promozione Crociere 2024</h5>
+                    <p class="card-text">Risparmia il 20% prenotando ora la tua crociera per il 2024. Scopri itinerari esclusivi e vivi un'esperienza indimenticabile a bordo.</p>
+                </div>
+                </div>
+            </div>
+            <!-- Card 3 -->
+            <div class="col-sm-12 col-md-4 mb-4">
+                <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top rounded-circle mx-auto d-block mt-3" alt="..." style="width: 100px; height: 100px;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Avventura in Montagna</h5>
+                    <p class="card-text">Vivi l'avventura con la nostra offerta su misura per gli amanti della montagna: per ogni pacchetto prenotato, un ski pass omaggio per goderti al meglio le piste!</p>
+                </div>
+                </div>
+            </div>
+        </div>
+
     </section>
 
 
