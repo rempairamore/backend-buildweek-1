1589a34d36bb32baf9fabbd4c24eb668d81ef900
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-27 12:14:03 +0100

    search

--- customtheme/search.php
+++ customtheme/search.php
@@ -2,16 +2,16 @@
     get_header();
 
     global $wp_query;
-    echo "<pre>";
-    print_r($wp_query);
-    echo "</pre>";
+    // echo "<pre>";
+    // print_r($wp_query);
+    // echo "</pre>";
     if (have_posts()) {
         while (have_posts()) {
             the_post();
 ?>
             <div style="position: relative;">
             
-                    <img class="imgArticle" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fviaggi.corriere.it%2Feuropa%2Fislanda%2F&psig=AOvVaw02v5NBOA1SZcxhz0FseSjk&ust=1709117229220000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIC8kvyry4QDFQAAAAAdAAAAABAE" alt="...">
+                    <img class="imgArticle" src="https://static2-viaggi.corriereobjects.it/wp-content/uploads/2015/06/kirkjufell-islanda-istock.jpg.webp?v=1689967365" alt="...">
              
                 <div class="titoloArticolo text-center position-absolute bottom-0 start-0 w-100">
                     <h1><?php echo $wp_query->query->s; ?></h1>
