e5387c874e587002b24d0f9d6725d9d41a56f07d
Matteo Nardin <nardin.matteo@gmail.com>
2024-02-27 13:01:57 +0100

    search

--- customtheme/archive.php
+++ customtheme/archive.php
@@ -24,7 +24,7 @@
                             while (have_posts()) {
                                 the_post();
                     ?>
-                        <div class="serach-article-content">
+                        <div class="serach-article-content mb-5">
                             <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                             <p><?php echo '<div>Category: ' . get_the_category_list( ', ' ) . '</div>'; ?></p>
                             <p class="author">Autore: <?php the_author(); ?> <span><i class="bi bi-calendar-week"></i> <?php the_date("d-m-o"); ?></span></p>
